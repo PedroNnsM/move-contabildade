b9185049b27b3109b9ef3eddc721e15c22b1ecb6
PedroNnsM <pedronunes95@outlook.com>
2022-12-19 23:04:34 -0300

    Vite + Tailwind

---
+++ my-project/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="text-3xl font-bold bg-gray-500 h-screen">
+      <h1>Hello world</h1>
+    </div>
+  )
+}
--- src/main.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', function(){
-    header.classList.toggle("sticky", window.scrollY > 0);
-    });
-  
-   function Toggle() {
-    header.classList.toggle("active");
-   } 
