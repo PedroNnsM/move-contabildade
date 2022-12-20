68214de0cb78c644f721be9a91ab513be67874ad
PedroNnsM <pedronunes95@outlook.com>
2022-12-19 23:26:30 -0300

    comentarios

--- my-project/src/components/Header/index.jsx
+++ my-project/src/components/Header/index.jsx
@@ -5,6 +5,7 @@ export function Header(){
   return(
     <nav className="flex justify-center items-center  ">
       <img src="http://github.com/pedronnsm.png" />
+      {/* Montando a lista de itens para nevegacao da Lp */}
       <ul className="flex flex-col text-center gap-4 md:flex-row my-4 ">
         <li className="border hover:opacity-60"><a href="#">Home</a></li>
         <li className="border hover:opacity-60"><a href="#">sobre move</a></li>
