c8295074ba220880793610c200d47eeb25031b5f
PedroNnsM <pedronunes95@outlook.com>
2022-12-23 13:37:36 -0300

    Header criado

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,11 @@
+import { Header } from "./components/Header";
 import {  Navbar } from "./components/Navbar";
 
 export default function App() {
   return (
     <div className="text-3xl font-bold bg-gray-500 h-screen">
       <Navbar />
-      
+      <Header />
     </div>
   )
 }
---
+++ my-project/src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+
+export function Header(){
+  return(
+    <div className="mx-4">
+      <h1>
+      Vamos montar a base da move ?</h1>
+    </div>
+  )
+}
