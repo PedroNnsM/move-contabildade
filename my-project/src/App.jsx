8fb399972b24b0efbdf5d3ee6083d85283454b57
PedroNnsM <pedronunes95@outlook.com>
2023-01-13 20:34:22 -0300

    Componente Preco criado

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -8,8 +8,14 @@ export default function App() {
     <div className="text-3xl font-bold bg-move-gray-100 h-screen">
       <Navbar />
       <Header />
-      <PriceCards />
-      
+      <div className="bg-white dark:bg-gray-900">
+        <div className="container px-6 py-8 mx-auto">
+          <div className="flex flex-col items-center justify-center space-y-8 lg:-mx-4 lg:flex-row lg:items-stretch lg:space-y-0">
+            <PriceCards />
+            <PriceCards />
+          </div>
+        </div>
+      </div>
 
 
       <FooterPage />
