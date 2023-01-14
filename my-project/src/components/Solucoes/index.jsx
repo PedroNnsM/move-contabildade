272d7e790c252e19f695c715be5010b4c50587cc
PedroNnsM <pedronunes95@outlook.com>
2023-01-13 23:57:36 -0300

    Link dos botoes

--- my-project/src/components/Solucoes/index.jsx
+++ my-project/src/components/Solucoes/index.jsx
@@ -27,7 +27,7 @@ export function PriceCards({servico, preco, periodo,passo1, passo2, passo3,textB
           </span>
         </div>
         <button className="text-move-gray-100 bg-move-blue-900 xl  rounded-xl text-sm text-center lg:text-base group">
-            <span className='block box-border border-2 border-move-blue-900 rounded-xl py-3 px-6 bg-move-blue-500  text-move-gray-100 translate-y-[-0.2em] ease-in duration-100 group-hover:translate-y-[-0.33em] group-active:translate-y-0'>{textBtn}</span> 
+            <a className='block box-border border-2 border-move-blue-900 rounded-xl py-3 px-6 bg-move-blue-500  text-move-gray-100 translate-y-[-0.2em] ease-in duration-100 group-hover:translate-y-[-0.33em] group-active:translate-y-0'href='https://api.whatsapp.com/send?phone=5547988856568&text=Ol%C3%A1,%20quero%20conhecer%20mais%20os%20servi%C3%A7os%20da%20Move!' target='_blank'>{textBtn}</a> 
           </button>
       </div>
 
