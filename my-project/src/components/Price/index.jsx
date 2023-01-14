df59e353ddd72a236731d9ea120aa1e02d2d99e0
PedroNnsM <pedronunes95@outlook.com>
2023-01-13 22:06:31 -0300

    Solocoes concluidas

--- my-project/src/components/Price/index.jsx
+++ my-project/src/components/Price/index.jsx
@@ -1,4 +1,4 @@
-export function PriceCards({servico, preco, periodo, }){
+export function PriceCards({servico, preco, periodo,passo1, passo2, passo3,textBtn }){
   return (
 <div className="flex flex-col w-full max-w-sm p-8 space-y-8 text-center bg-move-white border-2 border-move-blue-900 rounded-3xl lg:mx-4 shadow-2xl shadow-move-blue-900 ">
         <div className="flex-shrink-0">
@@ -6,31 +6,29 @@ export function PriceCards({servico, preco, periodo, }){
             {servico}
           </h2>
         </div>
+        
+        <ul className="flex-1 space-y-4">
+          <li className="text-gray-500 dark:text-sm">
+            {passo1}
+          </li>
+          <li className="text-gray-500 dark:text-sm">
+            {passo2}
+          </li>
+          <li className="text-gray-500 dark:text-sm">
+            {passo3}
+          </li>
+        </ul>
         <div className="flex-shrink-0">
-          <span className="pt-2 text-4xl font-bold text-gray-800 uppercase dark:text-gray-100">
+          <span className="pt-2 text-4xl font-bold text-move-blue-500 uppercase dark:text-gray-100">
             {preco}
           </span>
           <span className="text-gray-500 dark:text-gray-400">
             {periodo}
           </span>
         </div>
-        <ul className="flex-1 space-y-4">
-          <li className="text-gray-500 dark:text-gray-400">
-            Up to 10 projects
-          </li>
-          <li className="text-gray-500 dark:text-gray-400">
-            Up to 20 collaborators
-          </li>
-          <li className="text-gray-500 dark:text-gray-400">
-            10Gb of storage
-          </li>
-          <li className="text-gray-500 dark:text-gray-400">
-            Real-time collaborations
-          </li>
-        </ul>
-        <button className="inline-flex items-center justify-center px-4 py-2 font-medium text-white uppercase transition-colors bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none">
-          Start free trial
-        </button>
+        <button className="text-move-gray-100 bg-move-blue-900 xl  rounded-xl text-sm text-center lg:text-base group">
+            <span className='block box-border border-2 border-move-blue-900 rounded-xl py-3 px-6 bg-move-blue-500  text-move-gray-100 translate-y-[-0.2em] ease-in duration-100 group-hover:translate-y-[-0.33em] group-active:translate-y-0'>{textBtn}</span> 
+          </button>
       </div>
 
 );
