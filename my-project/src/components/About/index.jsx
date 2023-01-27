0299002e8affc9dc802c8eab8216fba3b17197e3
PedroNnsM <pedronunes95@outlook.com>
2023-01-26 23:39:37 -0300

    troca de frase feita

--- my-project/src/components/About/index.jsx
+++ my-project/src/components/About/index.jsx
@@ -5,10 +5,10 @@ export function About() {
 
       <section id='sobre' className=" bg-move-blue-900">
         <div className="max-w-6xl px-6 py-10 mx-auto">
-          <p className="text-xl font-medium text-move-blue-500 ">Sobre a Move</p>
-          <h1 className="mt-2 text-3xl font-semibold  capitalize lg:text-4xl text-move-white">
+          <p className="text-xl font-medium text-move-blue-500 ">Sobre a MOVE</p>
+          <h5 className="mt-2 text-3xl font-semibold  capitalize lg:text-4xl text-move-white">
             Quem somos ?
-          </h1>
+          </h5>
           <main className="relative z-20 w-full mt-8 md:flex md:items-center xl:mt-12">
             <div className="absolute w-full bg-move-blue-500 -z-10 md:h-96 rounded-2xl" />
             <div className="w-full p-6 bg-move-blue-500 md:flex md:items-center rounded-2xl md:bg-transparent md:p-0 lg:px-12 md:justify-evenly">
--- my-project/src/components/animation/TextAnimation.jsx
+++ my-project/src/components/animation/TextAnimation.jsx
@@ -1,16 +1,35 @@
+import { useEffect, useState } from 'react'
+import { useInterval } from 'react-use';
+
 import './style.css'
 
-import { useEffect, useState} from 'react'
-{/* <LiHeader text='Sem burocracia?' />
-          <LiHeader text='Que fala a sua língua?' />
-          <LiHeader text='Inovadora?' />
-          <LiHeader text='Impulsiona a sua empresa?' />
-          <LiHeader text='Planeja com você?' /> */}
-export default function TextAnimation(){
-  const queFalaASuaLiguaArray = "Planeja com você?".split("");
+const phrases = [
+  { text: "Que fala a sua língua?", array: "Que fala a sua língua?".split("") },
+  { text: "Inovadora?", array: "Inovadora?".split("") },
+  { text: "Impulsiona a sua empresa?", array: "Impulsiona a sua empresa?".split("") },
+  { text: "Planeja com você?", array: "Planeja com você?".split("") }
+]
+
+export default function TextAnimation() {
+  const [currentPhrase, setCurrentPhrase] = useState(phrases[3]);
+  const [count, setCount] = useState(0);
+  const [play, setPlay] = useState(true)
+
+  useInterval(
+    () => {
+      setCurrentPhrase(phrases[count]);
+      setCount((count + 1) % phrases.length);
+    },
+    play ? 6000 : null
+  )
+
+  useEffect(() => {
+    console.log('A frase atual foi alterada para: ', currentPhrase.text)
+  }, [currentPhrase])
+
   return (
     <div className=" flex gap-1  ">
-      {queFalaASuaLiguaArray.map((item, index) => (
+      {currentPhrase.array.map((item, index) => (
         <span key={index} className={`animate-fadeIn opacity-0 `} >{item}</span>
       ))}
     </div>
